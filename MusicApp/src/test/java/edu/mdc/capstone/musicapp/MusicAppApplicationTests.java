131955f1930d684e7b66c745b30afa213f387230
Alexander Justiniano <alexan.justiniano001@mymdc.net>
2025-01-23 11:49:35 -0500

    first commit, added a few base dependencies for the project

---
+++ MusicApp/src/test/java/edu/mdc/capstone/musicapp/MusicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mdc.capstone.musicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
