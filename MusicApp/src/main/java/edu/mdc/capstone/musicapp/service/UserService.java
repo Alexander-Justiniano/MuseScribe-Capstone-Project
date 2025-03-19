4b4c76ea32a7d9c7221912c4cc69defff8e11114
Osniel Torres <osniel-torres@hotmail.com>
2025-03-18 20:52:05 -0400

    migrated code to use spring data mongodb and removed old code

---
+++ MusicApp/src/main/java/edu/mdc/capstone/musicapp/service/UserService.java
@@ -0,0 +1,32 @@
+package edu.mdc.capstone.musicapp.service;
+
+import edu.mdc.capstone.musicapp.models.User;
+import edu.mdc.capstone.musicapp.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+import java.util.Date;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user) {
+        user.setCreatedAt(new Date());
+        return userRepository.save(user);
+    }
+
+    public Optional<User> getUserById(String id) {
+        return userRepository.findById(id);
+    }
+
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String id) {
+        userRepository.deleteById(id);
+    }
+}
